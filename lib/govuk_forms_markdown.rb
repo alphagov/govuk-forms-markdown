0e96885fbe86f3af0e15ef542c103b144108579d
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-08-15 17:55:25 +0100

    All links should open in a new tab
    fixes https://github.com/alphagov/govuk-forms-markdown/issues/9

--- lib/govuk_forms_markdown.rb
+++ lib/govuk_forms_markdown.rb
@@ -7,7 +7,7 @@ module GovukFormsMarkdown
   class Error < StandardError; end
 
   def self.render(markdown)
-    renderer = GovukFormsMarkdown::Renderer.new({ link_attributes: { class: "govuk-link" } })
+    renderer = GovukFormsMarkdown::Renderer.new({ link_attributes: { class: "govuk-link", rel: "noreferrer noopener", target: "_blank" } })
     Redcarpet::Markdown.new(renderer, no_intra_emphasis: true).render(markdown).strip
   end
 end
