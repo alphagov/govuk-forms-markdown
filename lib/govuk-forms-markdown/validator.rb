9fe93a4ba52745f1a2e78e063644cfc5456bcadb
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-08-29 15:11:08 +0100

    Add validation to log errors when unsupported markdown syntax used

--- lib/govuk-forms-markdown/validator.rb
+++ lib/govuk-forms-markdown/validator.rb
@@ -3,6 +3,8 @@
 require "redcarpet"
 require "redcarpet/render_strip"
 
+require_relative "./renderer"
+
 module GovukFormsMarkdown
   class Validator
     attr_accessor :markdown, :errors
@@ -15,6 +17,14 @@ module GovukFormsMarkdown
       markdown_without_syntax = Redcarpet::Markdown.new(Redcarpet::Render::StripDown).render(markdown).strip
 
       :too_long if markdown_without_syntax.length >= 5000
+    end
+
+    def validate_tags
+      return nil if markdown.nil? || markdown.empty?
+
+      renderer = GovukFormsMarkdown::Renderer.new({ link_attributes: { class: "govuk-link", rel: "noreferrer noopener", target: "_blank" } })
+      Redcarpet::Markdown.new(renderer, no_intra_emphasis: true).render(markdown)
+      renderer.errors if renderer.errors.any?
     end
   end
 end
--- spec/govuk-forms-markdown/validator/validator_spec.rb
+++ spec/govuk-forms-markdown/validator/validator_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe GovukFormsMarkdown::Validator do
   subject(:validator) { described_class.new(markdown) }
 
   describe "#validate_length" do
-
     context "when markdown is empty string" do
       let(:markdown) { "" }
 
@@ -39,7 +38,52 @@ RSpec.describe GovukFormsMarkdown::Validator do
         expect(validator.validate_length).to eq(:too_long)
       end
     end
+  end
 
+  describe "#validate_tags" do
+    context "when markdown is empty string" do
+      let(:markdown) { "" }
+
+      it "returns nil" do
+        expect(validator.validate_tags).to be_nil
+      end
+    end
+
+    context "when markdown is using supported tags" do
+      let(:markdown) { "## Supported heading levels" }
+
+      before do
+        # Stub out the GovukFormsMarkdown::Renderer.new and its methods
+        renderer_double = instance_double(GovukFormsMarkdown::Renderer, errors: [])
+        allow(GovukFormsMarkdown::Renderer).to receive(:new).and_return(renderer_double)
+
+        # Stub out Redcarpet::Markdown.new to return a double
+        markdown_double = instance_double(Redcarpet::Markdown, render: nil)
+        allow(Redcarpet::Markdown).to receive(:new).and_return(markdown_double)
+      end
+
+      it "returns nil" do
+        expect(validator.validate_tags).to be_nil
+      end
+    end
+
+    context "when markdown is using unsupported tags" do
+      let(:markdown) { "# Invalid [markdown]" }
+
+      before do
+        # Stub out the GovukFormsMarkdown::Renderer.new and its methods
+        renderer_double = instance_double(GovukFormsMarkdown::Renderer, errors: [:unsupported_tags_used])
+        allow(GovukFormsMarkdown::Renderer).to receive(:new).and_return(renderer_double)
+
+        # Stub out Redcarpet::Markdown.new to return a double
+        markdown_double = instance_double(Redcarpet::Markdown, render: nil)
+        allow(Redcarpet::Markdown).to receive(:new).and_return(markdown_double)
+      end
+
+      it "returns all errors" do
+        expect(validator.validate_tags).to eq([:unsupported_tags_used])
+      end
+    end
   end
 end
 # rubocop:enable RSpec/FilePath
