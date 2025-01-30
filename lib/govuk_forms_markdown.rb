7d5fa205377fd659c55b54f81268320e9a68a030
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-01-30 14:51:10 +0000

    Bump supported minimum version of Ruby to 3.4.1

--- lib/govuk_forms_markdown.rb
+++ lib/govuk_forms_markdown.rb
@@ -8,11 +8,11 @@ module GovukFormsMarkdown
   class Error < StandardError; end
 
   def self.render(markdown, allow_headings: true)
-    renderer = GovukFormsMarkdown::Renderer.new({ link_attributes: { class: "govuk-link", rel: "noreferrer noopener", target: "_blank" } }, allow_headings: allow_headings)
+    renderer = GovukFormsMarkdown::Renderer.new({ link_attributes: { class: "govuk-link", rel: "noreferrer noopener", target: "_blank" } }, allow_headings:)
     Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, disable_indented_code_blocks: true).render(markdown).strip
   end
 
   def self.validate(markdown, allow_headings: true)
-    GovukFormsMarkdown::Validator.new(markdown, allow_headings: allow_headings).validate
+    GovukFormsMarkdown::Validator.new(markdown, allow_headings:).validate
   end
 end
